1f232b85b7caa36e69b02ee368d77933c9635cee
anujjaha <yash@yash.local>
2017-04-30 12:11:45 +0530

    Work on Discover View

---
+++ Bonfire/Bonfire/TabbarVC/TabBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TabBarViewController.swift
+//  VIZI
+//
+//  Created by Bhavik on 16/12/16.
+//  Copyright © 2016 GWBB. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController
+{
+    override func viewDidLoad()
+    {
+        
+    }
+    override func viewDidAppear(_ animated: Bool)
+    {
+        
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController)
+    {
+        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
